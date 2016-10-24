c5cc2b05347787fb3b41f9488579af164daf7ff0
Artem_Pryzhkov <artem_pryzhkov@gmail.com>
2016-10-24 16:38:15 +0300

    Add second runner

---
+++ src/main/java/ua/tasks/ap/AppRunner2.java
@@ -0,0 +1,13 @@
+package ua.tasks.ap;
+
+/**
+ * Created by Artem_Pryzhkov on 24-Oct-16.
+ */
+public class AppRunner2 {
+    public static void main(String[] args) {
+        System.out.println("START PROGRAM2:");
+        for(String arg:args){
+            System.out.println(arg);
+        }
+    }
+}
