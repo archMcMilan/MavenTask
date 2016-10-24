1be4ff9ec1112609160020ed8139bac4ee48a250
Artem_Pryzhkov <artem_pryzhkov@gmail.com>
2016-10-24 16:30:13 +0300

    add plugin execution

---
+++ src/main/java/ua/tasks/ap/AppRunner.java
@@ -0,0 +1,13 @@
+package ua.tasks.ap;
+
+/**
+ * Created by Artem_Pryzhkov on 24-Oct-16.
+ */
+public class AppRunner {
+    public static void main(String[] args) {
+        System.out.println("START PROGRAM:");
+        for(String arg:args){
+            System.out.println(arg);
+        }
+    }
+}
