07520f27bc0f41d6e19601e11b43780635856edf
Artem_Pryzhkov <artem_pryzhkov@gmail.com>
2016-10-24 17:59:48 +0300

    Add ITest and Test Create in pom.xml 3 profiles -   testingITest - execute only ITest   testingWithoutITest - execute all except ITest   testing all - execute all

---
+++ src/test/java/ua/tasks/ap/ITest.java
@@ -0,0 +1,13 @@
+package ua.tasks.ap;
+
+import org.junit.Test;
+
+/**
+ * Created by Artem_Pryzhkov on 24-Oct-16.
+ */
+public class ITest {
+    @Test
+    public void test(){
+        System.out.println("Run ITest");
+    }
+}
---
+++ src/test/java/ua/tasks/ap/Test.java
@@ -0,0 +1,11 @@
+package ua.tasks.ap;
+
+/**
+ * Created by Artem_Pryzhkov on 24-Oct-16.
+ */
+public class Test {
+    @org.junit.Test
+    public void test(){
+        System.out.println("Run Test");
+    }
+}
