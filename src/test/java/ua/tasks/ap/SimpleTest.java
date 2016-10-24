3623ac3ea6e0c26f4203b85a69c7b4a6e5d3f0c3
Artem_Pryzhkov <artem_pryzhkov@gmail.com>
2016-10-24 18:13:48 +0300

    Add simpleTest

--- src/test/java/ua/tasks/ap/SimpleTest.java
+++ src/test/java/ua/tasks/ap/SimpleTest.java
@@ -1,18 +1,17 @@
 package ua.tasks.ap;
-
 import org.junit.Test;
-
 /**
  * Created by Artem_Pryzhkov on 24-Oct-16.
  */
-public class ITest {
-    @Test
-    public void test(){
-        System.out.println("Run ITest");
-    }
+public class SimpleTest {
 
     @Test
+    public void test(){
+        System.out.println("Run SimpleTest");
+    }
+    
+    @Test
     public void simpleTest(){
-        System.out.println("Run simple ITest");
+        System.out.println("Run simple SimpleTest");
     }
 }
